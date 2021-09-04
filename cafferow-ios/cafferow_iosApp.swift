31444b47782cc1d1e4553f49e4c07e63728a2123
nmurata <nmurata73@gmail.com>
2021-09-04 19:04:55 +0900

    Initial Commit

---
+++ cafferow-ios/cafferow_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  cafferow_iosApp.swift
+//  cafferow-ios
+//
+//  Created by Naoyuki Murata on 2021/09/04.
+//
+
+import SwiftUI
+
+@main
+struct cafferow_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
