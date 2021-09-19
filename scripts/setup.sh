77cb5440c6200eef901e0f21bfa8fbb984289448
nmurata <nmurata73@gmail.com>
2021-09-20 02:47:34 +0900

    build swiftgen

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,8 @@
 function build_cli_tools {
   echo "👻  Build SwiftLint👻"
   swift build -c release --package-path Tools --product swiftlint
+  echo "👻  Build SwiftGen👻"
+  swift build -c release --package-path Tools --product swiftgen
 }
 
 function setup() {
