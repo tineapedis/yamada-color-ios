e2cac779de296d16b4dbe622836ca34a7648cecf
nmurata <nmurata73@gmail.com>
2021-09-24 02:16:04 +0900

    impl ColorSelectCore

--- yamada-color-ios/Application/ColorSelect/ColorSelectCore.swift
+++ yamada-color-ios/Application/ColorSelect/ColorSelectCore.swift
@@ -9,20 +9,40 @@
 import ComposableArchitecture
 
 struct ColorSelectState: Equatable {
-
+    var purpleYamadaColor = YamadaColorState(yamadaColor: .init(defaultType: .purple))
+    var yellowYamadaColor = YamadaColorState(yamadaColor: .init(defaultType: .yellow))
+    var blackYamadaColor = YamadaColorState(yamadaColor: .init(defaultType: .black))
+    var pinkYamadaColor = YamadaColorState(yamadaColor: .init(defaultType: .pink))
 }
 
 enum ColorSelectAction {
-    case didTapColorButton
+    case purpleYamadaColor(YamadaColorAction)
+    case yellowYamadaColor(YamadaColorAction)
+    case blackYamadaColor(YamadaColorAction)
+    case pinkYamadaColor(YamadaColorAction)
 }
 
 struct ColorSelectEnvironment {}
 
 let colorSelectReducer = Reducer<ColorSelectState, ColorSelectAction, ColorSelectEnvironment>.combine(
-    Reducer { _, action, _ in
-        switch action {
-        case .didTapColorButton:
-            return .none
-        }
-    }
+    yamadaColorReducer.pullback(
+        state: \ColorSelectState.purpleYamadaColor,
+        action: /ColorSelectAction.purpleYamadaColor,
+        environment: { _ in YamadaColorEnvironment() }
+    ),
+    yamadaColorReducer.pullback(
+        state: \ColorSelectState.yellowYamadaColor,
+        action: /ColorSelectAction.yellowYamadaColor,
+        environment: { _ in YamadaColorEnvironment() }
+    ),
+    yamadaColorReducer.pullback(
+        state: \ColorSelectState.blackYamadaColor,
+        action: /ColorSelectAction.blackYamadaColor,
+        environment: { _ in YamadaColorEnvironment() }
+    ),
+    yamadaColorReducer.pullback(
+        state: \ColorSelectState.pinkYamadaColor,
+        action: /ColorSelectAction.pinkYamadaColor,
+        environment: { _ in YamadaColorEnvironment() }
+    )
 )
